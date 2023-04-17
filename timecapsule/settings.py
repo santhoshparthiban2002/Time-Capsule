45dc20d96779b13d44ff41593a0572531c34d10a
santhoshparthiban <santhoshparthiban2002@gmail.com>
2023-04-17 19:05:07 +0530

    removed conflicts

--- timecapsule/settings.py
+++ timecapsule/settings.py
@@ -86,7 +86,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
-STATIC_ROOT  =(os.path.join(BASE_DIR, 'staticfiles_build','static'),)
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT  = os.path.join(BASE_DIR,'staticfiles_build','static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 
