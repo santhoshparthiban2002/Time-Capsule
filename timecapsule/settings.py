d77f05f45aac54f95c0a3c8c964301c250959b1a
santhoshparthiban <santhoshparthiban2002@gmail.com>
2023-04-17 17:38:29 +0530

    Corrected conflicts

--- timecapsule/settings.py
+++ timecapsule/settings.py
@@ -87,7 +87,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
-STATICFILES_DIRS =(os.path.join(BASE_DIR,'staticfiles_build','static'),)
+STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 
