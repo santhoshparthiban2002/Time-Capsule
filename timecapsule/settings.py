deae41dcd143673b35b81d74c22f5176003fdb12
santhoshparthiban <santhoshparthiban2002@gmail.com>
2023-04-17 17:32:04 +0530

    Corrected conflicts

--- timecapsule/settings.py
+++ timecapsule/settings.py
@@ -55,12 +55,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'timecapsule.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+   # 'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+ #       'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 AUTH_PASSWORD_VALIDATORS = [
